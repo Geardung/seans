1d246b8326a9006e906a799f966962f94a4052ba
Geardung <Geardung@ya.ru>
2026-09-17 14:14:14 +0300

    chore: switch S3 to Beget, add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/watchin-mokchin/',
+  base: '/',
   server: {
     proxy: {
       '/s3': {
-        target: 'https://s3.regru.cloud/watchin-mokchin',
+        target: 'https://s3.ru1.storage.beget.cloud/e7ad1529f5f1-watchin-mokchin',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/s3/, ''),
       },
